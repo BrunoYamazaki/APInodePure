5db33e0a1047fd095e01c0956d26f72e3478be00
Bruno <brunogsm45@gmail.com>
2022-06-16 14:10:49 -0300

    first commit

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Bruno',
+  },
+  {
+    id: 2,
+    name: 'Edenilson',
+  },
+  {
+    id: 3,
+    name: 'Gisele',
+  },
+]
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const UserController = require('./Controllers/UserController')
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+  {
+    endpoint: `/users/:id`,
+    method: 'GET',
+    handler: UserController.getUserById,
+  },
+];
